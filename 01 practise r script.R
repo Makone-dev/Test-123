3f6b2fc303a8daf78abfcc47818c03404ce2804d
Makone Maja <makonemaja@Makones-MacBook-Air.local>
2026-02-21 09:19:36 +0100

    First practise script after lesson

---
+++ 01 practise r script.R
@@ -0,0 +1,45 @@
+#install.packages("tidyverse")
+
+
+# Practice session 01 ----
+
+## Values ----
+
+1, "Lucerne", TRUE 
+
+## Objects ----
+ 
+x <- c(10/3, 2, 1.42)
+
+## Functions ----
+mean (x, na.rm = TRUE)
+
+library("tidyverse")
+view(mpg)
+
+## Example 1 ----
+
+mpg_rec <- select(mpg, displ, hwy)
+
+mpg %>% select(displ, hwy)
+
+mpg %>% filter(year == 1999)
+
+## Where and how to save files ----
+
+getwd()
+
+here : : here ()
+
+#install.packages("here")
+
+
+
+library("here")
+
+#Get wd:
+here::here()
+
+load(file = here::here("data", "dat_orig.Rdata"))
+
+load(file = here::here("data", "dat_orig.Rdata"))
